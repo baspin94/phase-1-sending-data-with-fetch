f9103fb36d32ea54ee2df263a12b096b79103fd3
Bianca Aspin <bianca@mj94.net>
2023-01-17 10:04:41 -0800

    Completed lab.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const userObject = {
+        name: userName,
+        email: userEmail
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userObject)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const newId = document.createElement("p");
+        newId.textContent = data.id;
+        document.body.append(newId);
+    })
+    .catch(error => {
+        const errorMsg = document.createElement("p");
+        errorMsg.textContent = error.message;
+        document.body.append(errorMsg);
+    })
+}
